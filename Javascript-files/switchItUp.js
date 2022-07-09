66d2cf9a0d824782c78f012da506e7becf24d3e3
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-07-10 00:23:19 +0200

    Categorize New Member

---
+++ Javascript-files/switchItUp.js
@@ -0,0 +1,67 @@
+// let number = 9;
+
+// switch (number) {
+//   case 1:
+//     number = "One";
+//     break;
+//   case 2:
+//     number = "Two";
+//     break;
+//   case 3:
+//     number = "Three";
+//     break;
+//   case 4:
+//     number = "Four";
+//     break;
+//   case 5:
+//     number = "Five";
+//     break;
+//   case 6:
+//     number = "Six";
+//     break;
+//   case 7:
+//     number = "Seven";
+//     break;
+//   case 8:
+//     number = "Eight";
+//     break;
+//   case 9:
+//     number = "Nine";
+//     break;
+
+//   default:
+//     a = "Not found";
+//     break;
+// }
+
+// console.log(number
+
+function switchItUp(number) {
+  //Write your own Code!
+
+  switch (number) {
+    case 1:
+      return "One";
+    case 2:
+      return "Two";
+    case 3:
+      return "Three";
+    case 4:
+      return "Four";
+    case 5:
+      return "Five";
+    case 6:
+      return "Six";
+    case 7:
+      return "Seven";
+    case 8:
+      return "Eight";
+    case 9:
+      return "Nine";
+
+    default:
+      a = "Not found";
+  }
+}
+
+console.log(switchItUp(5));
