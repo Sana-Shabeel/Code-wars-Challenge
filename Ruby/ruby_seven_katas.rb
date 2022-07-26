db2879e99419d16ae37f4066f22d1a57d8670862
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-07-26 10:18:36 +0200

    Convert binary to decimal

--- Ruby/ruby_seven_katas.rb
+++ Ruby/ruby_seven_katas.rb
@@ -17,4 +17,13 @@ def is_triangle(a,b,c)
    
 end
 
-p is_triangle(7,2,2)
+is_triangle(7,2,2)
+
+# Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+# Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+def binary_array_to_number(arr)
+    # your code here
+    arr.join('').to_i(2)
+end 
