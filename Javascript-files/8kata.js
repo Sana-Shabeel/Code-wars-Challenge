db3aaae7defa3119b1f91f7c78e978804985f339
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-11-02 17:09:10 +0100

    KATA: Removing Elements

--- Javascript-files/8kata.js
+++ Javascript-files/8kata.js
@@ -1,11 +1,19 @@
+// KATA 8 - code wars
+
 /*
-KATA 8 - code wars
+REMOVING ELEMENTS 
 
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
 
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
 */
-function removeEveryOther(arr) {
-  //your code here
-  return arr.filter((s) => s !== "Remove");
-}
 
+function removeEveryOther(arr) {
+  return arr.filter((c, i) => i % 2 === 0); // fiilters out the even elements
+}
 console.log(removeEveryOther(["Keep", "Remove", "Keep", "Remove", "Keep"]));
+
+////////////////////////////////////////////////////////////////////////////
