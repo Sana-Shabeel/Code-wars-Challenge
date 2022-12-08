2f4ffc6b71fdd4f770f40510a7fcb516ed0f23c4
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-08 19:21:48 +0300

    Mexican Wave pt1

---
+++ Javascript-files/QuickTest.js
@@ -0,0 +1,9 @@
+function wave(str) {
+  // create an array with (str.length) empty slots
+  // fill the slots with str
+  const arrStr = Array(str.length).fill(str);
+
+  return;
+}
+
+console.log(wave("hello"));
