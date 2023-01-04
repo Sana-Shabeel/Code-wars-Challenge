a85e31773187582d0d3c0699526aa79433999c49
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-05 01:54:36 +0300

    Simple pigLatin - solution

--- Javascript-files/5kata/pigLatin.js
+++ Javascript-files/5kata/pigLatin.js
@@ -4,3 +4,29 @@ Simple Pig Latin
 
 
 */
+
+function pigIt(str) {
+  //remove the first letter of EACH word and add 'ay' at the end
+  // Leave punctuation marks untouched.
+
+  // split the string into an array of words
+  const splitStr = str.split(" ");
+
+  const punctuation = ["!", ".", "?"];
+  let newStr = "";
+
+  for (let i = 0; i < splitStr.length; i++) {
+    const word = splitStr[i];
+    const newWord = word.slice(1) + word[0] + "ay ";
+
+    if (punctuation.includes(word)) {
+      //
+      console.log(word[0]);
+      newStr += word;
+      continue;
+    }
+    newStr += newWord;
+  }
+
+  return newStr.trimEnd();
+}
