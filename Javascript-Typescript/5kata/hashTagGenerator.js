2886d8cf1d7e23ee3bc188fc132dda04eb3e0d58
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-06-15 20:50:32 +0300

    Add The Hashtag Generator - kata 5

--- Javascript-Typescript/5kata/hashTagGenerator.js
+++ Javascript-Typescript/5kata/hashTagGenerator.js
@@ -17,3 +17,19 @@ Examples
 
 
 */
+
+function generateHashtag(str) {
+  console.log(str);
+  if (str.trim() === "") return false;
+  const arr = str.split(" ");
+  const filterEmptySpace = arr.filter((space) => space !== "");
+  const capitalized = filterEmptySpace.map(
+    (c) => c.at(0).toUpperCase() + c.slice(1)
+  );
+
+  return "#" + capitalized.join("");
+}
+
+console.log(generateHashtag("hello world"));
+console.log(generateHashtag("Codewars"));
+console.log(generateHashtag("code" + " ".repeat(140) + "wars"));
